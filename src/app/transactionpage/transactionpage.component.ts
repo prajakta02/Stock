e2975184dd00b40f79d3eca23d9be49be992d167
Ashwin  Mohan <mohanashwin999@gmail.com>
2020-06-27 01:05:34 +0530

    mobile friendly , transaction reverse'

--- src/app/transactionpage/transactionpage.component.ts
+++ src/app/transactionpage/transactionpage.component.ts
@@ -13,12 +13,17 @@ import {NgxPaginationModule} from 'ngx-pagination';
 export class TransactionpageComponent implements OnInit {
 
   p:number=1;
+  isMobile = false;
 
   constructor(private transactionService: TransactionService) { }
 
   transactions: Transaction[];
   ngOnInit() {
     this.transactions = this.transactionService.getTransactions();
+    this.transactions = this.transactions.reverse();
+    if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+      this.isMobile = true;
+   }
   }
 
 }
